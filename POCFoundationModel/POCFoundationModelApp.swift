8f5dc493974c2e1718d141954b62400ffa08cbf4
Neon-Creation <pedrone0n350@gmail.com>
2026-08-06 10:26:49 -0300

    Initial Commit

---
+++ POCFoundationModel/POCFoundationModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCFoundationModelApp.swift
+//  POCFoundationModel
+//
+//  Created by Pedro Henrique Castro da Silva on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct POCFoundationModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
